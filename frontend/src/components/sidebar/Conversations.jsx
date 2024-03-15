b45ff094ffd3c240a0363de5b6b514d48432a62c
Arilala <haingoarilalarandrianjatovo@gmail.com>
2024-03-15 08:02:12 +0100

    add  zustand stats

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,11 +1,22 @@
+import useGetConversations from "../../hooks/useGetConversations";
 import Conversation from "./Conversation";
-
+import { getRandomEmoji } from "../../utils/emojis.js";
 export const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+  console.log({ loading, conversations });
   return (
     <div className="py-2 flex flex-col overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((conversation,) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+        />
+      ))}
+
+      {loading ? (
+        <span className="loading loading-spinner mx-auto"></span>
+      ) : null}
     </div>
   );
 };
